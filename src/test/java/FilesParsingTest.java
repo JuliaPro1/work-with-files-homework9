85da815cd68eda6bc13db9b621088cafd0104b8d
july <julpkolp@gmail.com>
2025-05-23 22:03:37 +0300

    Добавление Assertions от библиотеки AssertJ и проверки на наличие файлов в zip архиве

--- src/test/java/FilesParsingTest.java
+++ src/test/java/FilesParsingTest.java
@@ -13,6 +13,8 @@ import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
+import static com.codeborne.pdftest.assertj.Assertions.assertThat;
+
 public class FilesParsingTest {
     private ClassLoader cl = FilesParsingTest.class.getClassLoader();
 
@@ -20,30 +22,40 @@ public class FilesParsingTest {
     @Tag("FILE_TEST")
     @DisplayName("Проверка pdf файла из zip архива")
     void pdfTest() throws Exception {
+        boolean pdfFound = false;
         try (ZipInputStream zis = new ZipInputStream(
                 cl.getResourceAsStream("differentFiles.zip")
         )) {
             ZipEntry entry;
             while ((entry = zis.getNextEntry()) != null) {
                 if (entry.getName().endsWith(".pdf")) {
+                    pdfFound = true;
                     PDF pdf = new PDF(zis);
+                    assertThat(pdfFound).isTrue();
+                    assertThat(pdf.creator).contains("Writer");
+                    assertThat(pdf.numberOfPages).isEqualTo(4);
+                    /* JUnit's  asserts
                     Assertions.assertEquals("Writer", pdf.creator);
                     Assertions.assertEquals(4, pdf.numberOfPages);
+                    */
                 }
             }
         }
+
     }
 
     @Test
     @Tag("FILE_TEST")
     @DisplayName("Проверка xls файла из zip архива")
     void xlsTest() throws Exception {
+        boolean xlsxFound = false;
         try (ZipInputStream zis = new ZipInputStream(
                 cl.getResourceAsStream("differentFiles.zip")
         )) {
             ZipEntry entry;
             while ((entry = zis.getNextEntry()) != null) {
                 if (entry.getName().endsWith(".xlsx")) {
+                    xlsxFound = true;
                     XLS xls = new XLS(zis);
                     String actualValueString = xls.excel
                             .getSheetAt(0)
@@ -56,8 +68,13 @@ public class FilesParsingTest {
                             .getCell(6)
                             .getNumericCellValue();
 
+                    assertThat(xlsxFound).isTrue();
+                    assertThat(actualValueString).contains("Regular Air");
+                    assertThat(actualValueDouble).isEqualTo(9.11);
+                    /*JUnit's  asserts
                     Assertions.assertTrue(actualValueString.contains("Regular Air"));
                     Assertions.assertEquals(9.11, actualValueDouble);
+                     */
                 }
             }
         }
@@ -67,12 +84,14 @@ public class FilesParsingTest {
     @Tag("FILE_TEST")
     @DisplayName("Проверка scv файла из zip архива")
     void scvTest() throws Exception {
+        boolean scvFound = false;
         try (ZipInputStream zis = new ZipInputStream(
                 cl.getResourceAsStream("differentFiles.zip")
         )) {
             ZipEntry entry;
             while ((entry = zis.getNextEntry()) != null) {
                 if (entry.getName().endsWith(".csv")) {
+                    scvFound = true;
                     CSVReader csvReader = new CSVReader(new InputStreamReader(zis));
                     List<String[]> content = csvReader.readAll();
                     org.assertj.core.api.Assertions.assertThat(content).contains(
@@ -81,6 +100,7 @@ public class FilesParsingTest {
                             new String[]{"Kevin Millar", "BAL", "First Baseman"},
                             new String[]{"Chris Gomez", "BAL", "First Baseman"}
                     );
+                    assertThat(scvFound).isTrue();
                 }
             }
         }
--- src/test/java/JsonTest.java
+++ src/test/java/JsonTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class JsonTest {
@@ -23,13 +25,24 @@ public class JsonTest {
              Reader reader = new InputStreamReader(is)) {
             Author author = objectMapper.readValue(reader, Author.class);
 
+            assertThat(author.getName()).isEqualTo("Theodore Dreiser");
+            assertThat(author.getId()).isEqualTo(24236);
+            assertThat(author.getAveragePriceBook()).isEqualTo(114.36);
+            assertThat(author.getTotalQuantity()).isEqualTo(51);
+            assertThat(author.getBooks()).isNotNull();
+            assertThat(author.getBooks().size()).isEqualTo(3);
+
+
+       /* JUnit's  asserts
             assertEquals("Theodore Dreiser", author.getName());
             assertEquals(24236, author.getId());
             assertEquals(114.36, author.getAveragePriceBook());
             assertEquals(51, author.getTotalQuantity());
             assertNotNull(author.getBooks());
             assertEquals(3, author.getBooks().size());
-            assertTrue(author.getBooks().containsAll(List.of("The Financier", "Sister Carrie", "The Titan")));
+            assertThat(author.getBooks()).containsAll(List.of("The Financier", "Sister Carrie", "The Titan")));
+
+ */
         }
     }
 }
